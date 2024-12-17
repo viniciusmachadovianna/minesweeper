cd959162b5a8b45b14e56468405a7d4f1cd7cdb5
Vinícius Machado <viniciusmachadovianna@outlook.com>
2024-12-17 11:44:14 -0300

    createfield handle upgrade

--- script.js
+++ script.js
@@ -5,27 +5,26 @@ const diff = document.getElementById("difficulty"),
     medium: {squares: 256,flags: 40,},
     hard: {squares: 484,flags: 100,},
 };
-function createField(difficulty){
+function createField(size){
     minefield.replaceChildren();
+    let tiles=[];
     const frag = document.createDocumentFragment();
-    for(i=0;i<field[difficulty].squares; i++) {
+    for(i=0;i<field[size].squares; i++) {
         const tile = document.createElement('div');
         tile.value = i;
-        // minefield.innerHTML+=`<div id="s${i}"></div>`;
         if (i%2!==0){tile.style.background = "rgb(73, 170, 255)";}
         frag.appendChild(tile);
+        tiles.push(tile.value);
     }
     minefield.appendChild(frag);
-    let bombs = [];
-    while(bombs.length <field[diff.value].flags){
-        let randBomb = Math.floor(Math.random() * (field[diff.value].squares))+1;
-        if (!bombs.includes(randBomb)) {bombs.push(randBomb);}
+    for(i=tiles.length-1;i>0;i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [tiles[i], tiles[j]] = [tiles[j], tiles[i]];
     }
-    minefield.addEventListener('mouseover',(e)=>{
-        e.target.style.background = "rgb(159, 210, 255)";
-    })
-    minefield.addEventListener('mouseout',(e)=>{
-        e.target.style.background=e.target.value%2!==0?"rgb(73, 170, 255)":"";
-    })
+    let mines=tiles.slice(0,field[size].flags);
+    console.log(mines);
+    
+    minefield.addEventListener('mouseover',(e)=>{e.target.style.background="#9fd2ff";})
+    minefield.addEventListener('mouseout',(e)=>{e.target.style.background=e.target.value%2!==0?"#49aaff":"";})
 }
 createField(diff.value);
